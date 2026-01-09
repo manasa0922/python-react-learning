b4be2ce1abd22859d002fac065599927a600383c
Manasa S S <135146760+manasa0922@users.noreply.github.com>
2026-01-09 23:05:05 +0530

    file handling concepts

---
+++ Python/students.py
@@ -0,0 +1,19 @@
+# file create and write
+file  = open("students.txt", "w")
+file.write("Manasa,Manasa, 90,80, 90\n")
+file.write("Kavya,MSc, 80,90, 100\n")
+file.close()
+
+#append dat ato existing file
+file  = open("students.txt", "a")
+file.write("Megha,MSc, 80,90, 100\n")
+file.close()
+
+#Print data
+file = open("students.txt", "r")
+data = file.read()
+print(data)
+file.close()
+
+
+
